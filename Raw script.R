04634e397d4193b3db4fa11b8e1acb709f876883
Hakeem-7 <52264207+Hakeem-7@users.noreply.github.com>
2020-04-19 17:26:26 -0500

    Data exploration!

--- Raw script.R
+++ Raw script.R
@@ -1,1 +1,28 @@
 # let's gooo
+library(dplyr)
+library(tidyverse)
+library(DT)
+
+?heatmap
+
+listing <- as_tibble(read.csv("files/listings.csv"))
+head(listing)
+listing$latitude <- as.integer(listing$latitude)
+
+head(listing)
+
+max(listing$price)
+
+# Price range of each neighbourhood
+library("d3heatmap")
+price_var <- listing %>%
+  group_by(neighbourhood) %>%
+  summarise_each(funs(min(.,na.rm = T), max(., na.rm = T)),
+                 matches("pric")) %>% rename("price_min" = min, "price_max" = max) %>%
+  arrange(desc(price_max))%>%
+  DT::datatable()
+
+price_var
+
+price_var1 <- as.matrix(price_var)
+heatmap(price_var1)
